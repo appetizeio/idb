e553e410605871167d332e8b564127f6a82bfa3b
Rūdolfs Bundulis <rudolfs.bundulis@gmail.com>
2025-07-08 18:29:05 +0300

    Simplify the implementaton
    *) We can consume the data sync since the copying is the long operation not sending the response
    *) Put everything in one method so that nothing escapes scope
    *) Expose the hidden pushFrame to drive the frames

--- idb_companion/SwiftServer/MethodHandlers/FramebufferStreamMethodHandler.swift
+++ idb_companion/SwiftServer/MethodHandlers/FramebufferStreamMethodHandler.swift
@@ -19,68 +19,27 @@ struct FramebufferStreamMethodHandler {
 
   func handle(requestStream: GRPCAsyncRequestStream<Idb_FramebufferStreamRequest>, responseStream: GRPCAsyncResponseStreamWriter<Idb_FramebufferStreamResponse>, context: GRPCAsyncServerCallContext) async throws {
     @Atomic var finished = false
-    @Atomic var copyRequestQueue: [(name: String, length: UInt64, completion: (String, UInt64) -> Void)] = []
+    @Atomic var copyRequestQueue: [(name: String, length: UInt64)] = []
 
-    let framebufferStream = try await startFramebufferStream(
-      responseStream: responseStream,
-      finished: _finished,
-      copyRequestQueue: _copyRequestQueue)
-
-    let observeClientCancelStreaming = Task<Void, Error> {
-      for try await request in requestStream {
-        switch request.control {
-        case .stop:
-          return
-        case .copyFramebuffer(let copyRequest):
-          try await withCheckedThrowingContinuation { continuation in
-            _copyRequestQueue.sync { queue in
-              queue.append((
-                name: copyRequest.sharedMemoryName,
-                length: copyRequest.sharedMemoryLength,
-                completion: { name, bytesWritten in
-                  Task {
-                    let response = Idb_FramebufferStreamResponse.with {
-                      $0.sharedMemoryName = name
-                      $0.bytesWritten = bytesWritten
-                    }
-                    do {
-                      try await responseStream.send(response)
-                      continuation.resume()
-                    } catch {
-                      continuation.resume(throwing: error)
-                    }
-                  }
-                }
-              ))
-            }
-          }
-        case .none:
-          throw GRPCStatus(code: .invalidArgument, message: "Client should not close request stream explicitly, send `stop` frame first")
-        }
-      }
-    }
-
-    let observeFramebufferStreamStop = Task<Void, Error> { 
-      try await BridgeFuture.await(framebufferStream.completed)
-    }
-
-    try await Task.select(observeClientCancelStreaming, observeFramebufferStreamStop).value
-
-    try await BridgeFuture.await(framebufferStream.stopStreaming())
-    targetLogger.log("The framebuffer stream is terminated")
-  }
-
-  private func startFramebufferStream(responseStream: GRPCAsyncResponseStreamWriter<Idb_FramebufferStreamResponse>, finished: Atomic<Bool>, copyRequestQueue: Atomic<[(name: String, length: UInt64, completion: (String, UInt64) -> Void)]>) async throws -> FBVideoStream {
+    let streamWriter = FIFOStreamWriter(stream: responseStream)
     let consumer = FBBlockDataConsumer.synchronousDataConsumer { data in
-      guard !finished.wrappedValue else { return }
-      let copyRequest: (name: String, length: UInt64, completion: (String, UInt64) -> Void)?  = copyRequestQueue.sync { queue in
+      guard !_finished.wrappedValue else { return }
+      let copyRequest: (name: String, length: UInt64)?  = _copyRequestQueue.sync { queue in
         guard !queue.isEmpty else { return nil }
         return queue.removeFirst()
       }
-        
+          
       if let copyRequest = copyRequest {
         let bytesWritten = copyFrameToSharedMemory(data: data, sharedMemoryName: copyRequest.name, sharedMemoryLength: copyRequest.length)
-        copyRequest.completion(copyRequest.name, bytesWritten)
+        let response = Idb_FramebufferStreamResponse.with {
+          $0.sharedMemoryName = copyRequest.name
+          $0.bytesWritten = bytesWritten
+        }
+        do {
+          try streamWriter.send(response)
+        } catch {
+          _finished.set(true)
+        }
       }
     }
 
@@ -92,18 +51,45 @@ struct FramebufferStreamMethodHandler {
       avgBitrate: nil,
       keyFrameRate: nil
     )
-    let framebufferStream = try await BridgeFuture.value(target.createStream(with: config))
+    guard let simulatorVideoStream = try await BridgeFuture.value(target.createStream(with: config)) as? FBSimulatorVideoStream else { throw GRPCStatus(code: .internalError, message: "Framebuffer stream can only be used with a simulator device") };
 
-    try await BridgeFuture.await(framebufferStream.startStreaming(consumer))
+    try await BridgeFuture.await(simulatorVideoStream.startStreaming(consumer))
 
-    return framebufferStream
+    let observeClientCancelStreaming = Task<Void, Error> {
+      for try await request in requestStream {
+        switch request.control {
+        case .stop:
+          return
+        case .copyFramebuffer(let copyRequest):
+          _copyRequestQueue.sync { queue in
+            queue.append((
+              name: copyRequest.sharedMemoryName,
+              length: copyRequest.sharedMemoryLength
+            ))
+          }
+          simulatorVideoStream.pushFrame()
+          break
+        case .none:
+          throw GRPCStatus(code: .invalidArgument, message: "Client should not close request stream explicitly, send `stop` frame first")
+        }
+      }
+    }
+
+    let observeFramebufferStreamStop = Task<Void, Error> { 
+      try await BridgeFuture.await(simulatorVideoStream.completed)
+    }
+
+    try await Task.select(observeClientCancelStreaming, observeFramebufferStreamStop).value
+
+    try await BridgeFuture.await(simulatorVideoStream.stopStreaming())
+    targetLogger.log("The framebuffer stream is terminated")
   }
-    
+
   private func copyFrameToSharedMemory(data: Data, sharedMemoryName: String, sharedMemoryLength: UInt64) -> UInt64 {
     let bytesToCopy = min(UInt64(data.count), sharedMemoryLength)
-    let shmFd = shmopen(sharedMemoryName, O_RDWR, 0)
+    let shmFd = shmopen(sharedMemoryName, O_RDWR, 0666)
     if shmFd == -1 {
-      targetLogger.log("shm_open(\(sharedMemoryName), O_RDWR, 0) failed with \(errno)")
+      targetLogger.log("shm_open(\(sharedMemoryName), O_RDWR, 0666) failed with \(errno)")
       return 0
     }
         
